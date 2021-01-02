791d87239c5e0c49b8d09c1d74aa31df2c7866cf
1diaboli <diaboli_by@mail.ru>
2021-01-02 10:52:01 +0300

    the first commit

---
+++ src/com/javacourse/se/Main.java
@@ -0,0 +1,60 @@
+package com.javacourse.se;
+
+import com.javacourse.se.task3.StudentGroup;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        StudentGroup studentGroup = new StudentGroup("7А");
+//        studentGroup.setNamesOfStudents();
+//        studentGroup.getNamesOfStudents();
+//        System.out.println();
+//        studentGroup.setMarks();
+//        studentGroup.getMarksOfStudents();
+//        studentGroup.selectionSortOfMarks();
+//        System.out.println();
+//        studentGroup.setRandomMarks();
+//        studentGroup.getMarksOfStudents();
+//        System.out.println();
+//        StudentGroup.guessNumber();
+
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("VVedite text");
+        String s = scanner.nextLine();
+        char c = '1';
+        int count = 0;
+
+        String s1 = "a";
+        String s2;
+
+        StringBuilder stringBuilder = new StringBuilder("1");
+
+
+
+        for (int i = 0; i < 1_00; i++){
+            System.out.print(stringBuilder = stringBuilder.append(i));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/com/javacourse/se/task2/Area.java
@@ -0,0 +1,68 @@
+package com.javacourse.se.task2;
+
+
+public class Area {
+
+    private double length;
+    private double width;
+
+    public Area(){
+    }
+
+    public Area(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public void setLength(double length){
+        this.length = length;
+    }
+
+    public double getLength(){
+        return length;
+    }
+
+    public void setWidth(double width){
+        this.width = width;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    //  расчитывает площадь участка
+    public double getSquareArea(){
+        return length * width;
+    }
+
+    //  расчитывает периметр участка
+    public double getPerimeterArea(){
+        return (2 * length) + (2 * width);
+    }
+
+    //  переводит площадь участка м2 в сотки
+    public void convertToSotka (){
+        System.out.println("Площадь участка в сотках: " + length * width / 100 + " сотка");
+    }
+
+    //  переводит площадь участка м2 в га
+    public void convertToGa(){
+        System.out.println("Площадь участка в гектарах " + length * width / 10000 + " га");
+    }
+
+    //  сравнение площади участков
+    public void compare(Area area){
+        if (getSquareArea() > area.getSquareArea()){
+            System.out.println("Первый участок больше второго");
+        } else if(getSquareArea() < area.getSquareArea() ) {
+            System.out.println("Второй участок больше первого");
+        } else {
+            System.out.println("Участки равны");
+        }
+    }
+
+
+
+
+
+}
