73de500a3986bd09d80b477d900a41aa4ef71d7e
1diaboli <diaboli_by@mail.ru>
2021-01-16 19:42:02 +0300

    change Main class

--- src/com/javacourse/se/Main.java
+++ src/com/javacourse/se/Main.java
@@ -1,28 +1,9 @@
 package com.javacourse.se;
 
 
-import com.javacourse.se.task3.StudentGroup;
-import com.javacourse.se.task4.Brand;
-
 public class Main {
 
-
     public static void main(String[] args) {
-
-        StudentGroup studentGroup = new StudentGroup("7А");
-        studentGroup.setNamesOfStudents();
-        studentGroup.getNamesOfStudents();
-        System.out.println();
-        studentGroup.setMarks();
-        studentGroup.getMarksOfStudents();
-        studentGroup.selectionSortOfMarks();
-        System.out.println();
-        studentGroup.setRandomMarks();
-        studentGroup.getMarksOfStudents();
-        System.out.println();
-
-
-
 
     }
 }
